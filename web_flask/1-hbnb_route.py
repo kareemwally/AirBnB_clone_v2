fc33357ac39e95bc9784994bca9678c4598ca023
kareem <kareemwally@std.mans.edu.eg>
2024-07-28 01:48:22 +0300

    hbnb home

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+index and home pages in the website
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def main():
+    """ our index page """
+
+    return "<p>Hello HBNB!</p>"
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ the home page of HBNB"""
+    return "<p>HBNB</P>"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
