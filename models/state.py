c11dade9f09a3360a1ffd9258d6870b3133a5ae2
kareem <kareemwally@std.mans.edu.eg>
2024-07-28 16:23:28 +0300

    some shanges

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage_type
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class / table model"""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+    else:
+        name = ''
+
+    if storage_type != 'db':
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City instances"""
+            from models import storage
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
